b7b141a727ef83eaf794d1907e3ae74785f11416
Jing Liu <liujcdl@cn.ibm.com>
2019-10-11 20:01:00 +0800

    upgrade to 0.5.2

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-PLUGIN_VERSION=${PLUGIN_VERSION:-"0.5.1"}
+PLUGIN_VERSION=${PLUGIN_VERSION:-"0.5.2"}
 
 file="${HELM_PLUGIN_DIR:-"$(helm home)/plugins/helm-update-config"}/bin/helm-update-config"
 mkdir -p $(dirname ${file})
